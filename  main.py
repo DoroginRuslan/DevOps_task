68af764de9a6715a62323ae75f416642a178a68b
Ruslan Dorogin <dorogin.ruslan99@yandex.ru>
2021-02-01 00:48:27 +0300

    Добавлено тестирование на отсутствующие директории

---  main.py
+++  main.py
@@ -13,10 +13,10 @@ def get_program_args():
         exit(0)
 
 
-# Рекурсивная функция, формирует адреса папок
+# Рекурсивная функция, формирует адреса директорий
 # param[in] root_folder - корневая папка
 # param[in] deep - уровень вложенности папок для тестирования
-# return - список адресов к тестируемым папкам
+# return - список адресов к тестируемым директориям
 def gen_folders_path(root_folder, deep):
     result = []
     if deep > 0:
@@ -29,6 +29,37 @@ def gen_folders_path(root_folder, deep):
         return result
 
 
+# Функция отправляет директории на тестирования,
+# анализирует результаты, выводит данные в лог
+# param[in] folder_list - список тестируемых директорий
+def distribution_folders_to_test(folder_list):
+    for folder_path in folder_list:
+        test_folder(folder_path)
+
+
+# Функция тестирует папку и выводит ошибки в метсный файл лога
+# param[in] - тестируемая директория
+# return - результат тестирования True/False
+def test_folder(folder_path):
+    print("Тестируется папка: " + folder_path)
+    test_1 = test_check_exist_folders(folder_path, ["ft_reference", "ft_run"])
+    if test_1:
+        print("\tОтсутствующие директории: " + ''.join(test_1))
+
+
+# функция проверяет наличие папок в тестируемой директории
+# param[in] folder_path - тестируемая директория
+# param[in] list_in_dirs - папки, которые должны находиться внутри тестируемой директории
+# return - список отсутсвующих папок
+def test_check_exist_folders(folder_path, list_required_dirs):
+    result = []
+    list_path_dirs = os.listdir(folder_path)
+    for required_folder in list_required_dirs:
+        if required_folder not in list_path_dirs:
+            result.append(required_folder)
+    return result
+
+
 # Начало выполнения программы
-for test_path in gen_folders_path(get_program_args(), 1):
-    print(test_path)
+folders_list = gen_folders_path(get_program_args(), 1)
+distribution_folders_to_test(folders_list)
